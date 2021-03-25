46d34659d6f6d9924d888e4feaa734d76688fb46
gricardov <gricardov@gmail.com>
2021-03-24 19:47:51 -0500

    creditos agregados

--- src/data/data.js
+++ src/data/data.js
@@ -5,4 +5,4 @@ export const contactTypes = [{ type: 'WSP', icon: 'fab fa-whatsapp', name: 'What
 export const requestTypes = [{ type: 'DISENO', icon: 'fas fa-paint-brush', text: 'Diseños' }, { type: 'CRITICA', icon: 'fas fa-glasses', text: 'Críticas' }];
 export const requestStatuses = [{ id: 'DISPONIBLE', name: 'Nuevos', statistics: 0 }, { id: 'TOMADO', name: 'Elegidos', statistics: 0 }, { id: 'HECHO', name: 'Listos', statistics: 0 }];
 
-export const contributors = ['@Gricardov', '@JennyAline', '@CaroDePearolMorales', '@AlphaPhantom', '@JackDreamer'];
+export const contributors = ['@Gricardov', '@JennyAline', '@CaroDePearolMorales', '@AlphaPhantom', '@JackDreamer', '@PiccolaScrittrice', '@SayraBaylon'];
