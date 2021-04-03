af352b4a9a891930190b745e237d75c0d6351e7a
gricardov <gricardov@gmail.com>
2021-04-03 02:15:19 -0500

    portadas carrousel

--- src/componentes/home-panorama/panorama.js
+++ src/componentes/home-panorama/panorama.js
@@ -1,32 +1,62 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Navbar from '../navbar'
 import Fade from 'react-reveal/Fade';
-import ImgColaboracion from '../../img/colaboracion.svg';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleRight } from '@fortawesome/free-solid-svg-icons';
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+import { carrouselData } from '../../data/data';
 import './panorama.css'
 
 export const Panorama = () => {
+    const [index, setIndex] = useState(0);
+    useEffect(() => {
+        const interval = setInterval(() => {
+            if (index < carrouselData.length - 1) {
+                setIndex(index + 1);
+            } else {
+                setIndex(0);
+            }
+        }, 5000)
+        return () => {
+            clearInterval(interval)
+        }
+    }, [index])
+
+    const slide = carrouselData[index];
+
     return (
         <div className='panorama'>
             <Navbar startTransparent={true} />
-            <div className='container-xl container-header'>
-                <div className='container-controls-header'>
-                    <Fade left>
-                        <h1>Empieza a ser admirado por tu forma de escribir</h1>
-                    </Fade>
-                    <Fade left>
-                        <a target='_blank' rel="noreferrer" href='https://www.facebook.com/groups/1004324056570387' className='button button-green'>
-                            Únete al grupo
-                        {' '}
-                            <FontAwesomeIcon icon={faAngleRight} size='lg' />
-                        </a>
-                    </Fade>
-                </div>
-                <div className='img-container'>
-                    <img src={ImgColaboracion} className='img-colaboration' alt='img-colaboracion' />
-                </div>
-            </div>
+            <TransitionGroup className='panorama-slide-container'>
+
+                <CSSTransition
+                    classNames="panorama-slide"
+                    timeout={{ enter: 1000, exit: 1000 }}
+                    key={index}>
+
+                    <div className={`slide ${slide.orientation}`} style={{ background: slide.bg }}>
+                        <div className='slide-content'>
+                            <div className='container-controls-header'>
+                                <Fade left>
+                                    <h1>{slide.text}</h1>
+                                </Fade>
+                                <Fade left>
+                                    <a target='_blank' rel="noreferrer" href={slide.href} className='button button-green'>
+                                        {slide.buttonText}
+                                        {' '}
+                                        <FontAwesomeIcon icon={faAngleRight} size='lg' />
+                                    </a>
+                                </Fade>
+                            </div>
+                            <div className='img-container'>
+                                <img src={slide.img} className='img-colaboration' alt='img-colaboracion' />
+                            </div>
+                        </div>
+                    </div>
+                </CSSTransition>
+
+
+            </TransitionGroup>
         </div>
     )
 }
--- src/data/data.js
+++ src/data/data.js
@@ -1,3 +1,11 @@
+import ImgColaboracion from '../img/colaboracion.svg';
+
+export const carrouselData = [
+    { text: 'Primer concurso de cuentos de terror', buttonText: 'Participa', href: 'https://www.facebook.com/groups/1004324056570387', bg: 'radial-gradient(farthest-corner at 0 100%,rgb(0 0 1) 10%,#211b51 50%)', img: ImgColaboracion, orientation: 'right' },
+    { text: 'Portadas y críticas a tu medida', buttonText: 'Pide la tuya', href: 'https://www.facebook.com/groups/1004324056570387', bg: '#32c5bf', img: ImgColaboracion, orientation: 'right' },
+    { text: 'Empieza a ser admirado por tu forma de escribir', buttonText: 'Únete al grupo', href: 'https://www.facebook.com/groups/1004324056570387', bg: 'radial-gradient(farthest-corner at 0px 100%, rgb(82 70 198) 10%, rgba(139, 129, 236, 1) 50%)', img: ImgColaboracion }
+]
+
 export const critiquePoints = [{ id: 'INTENCION', name: 'Transmisión de mi intención', abrev: '¿Se entiende lo que quiero transmitir?' }, { id: 'ENGANCHE', name: 'Enganche de mi obra', abrev: '¿Qué tanto engancha mi obra?' }, { id: 'ORTOGRAFIA', name: 'Ortografía', abrev: '¿Qué tal es la ortografía?' }];
 export const designTypes = [{ type: 'CR', icon: 'fas fa-burn', text: 'Cuenta regresiva', tag: 'Nuevo', displayInDropdown: false }, { type: 'POR', icon: 'fas fa-book', text: 'Portada de libro' }, { type: 'BAN', icon: 'far fa-window-restore', text: 'Banner para post' }];
 export const contactTypes = [{ type: 'WSP', icon: 'fab fa-whatsapp', name: 'Whatsapp' }, { type: 'TLG', icon: 'fab fa-telegram', name: 'Telegram' }];
